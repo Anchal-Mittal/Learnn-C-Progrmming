4b9651c6d103feb5843fd764a853e8f949a9a659
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-02-01 18:35:21 +0530

    concept of getchar and putchar (input and output)

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+/* read one character at a time 
+return type of getchar  and putchar is integer
+*/
+
+
+#include<stdio.h>
+int main(){
+    int c;
+    printf("ENTER A VALUE = \n");
+    c =getchar();
+    putchar(c);
+    
+  }
+   
